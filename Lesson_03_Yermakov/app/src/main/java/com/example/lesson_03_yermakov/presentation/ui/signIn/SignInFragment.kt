f700fb0bb003a0c4059529e8f13de55f56e049fe
makniker <ermakov.nd@edu.spbstu.ru>
2023-11-05 13:49:06 +0300

    fixed error appearance

--- Lesson_03_Yermakov/app/src/main/java/com/example/lesson_03_yermakov/presentation/ui/signIn/SignInFragment.kt
+++ Lesson_03_Yermakov/app/src/main/java/com/example/lesson_03_yermakov/presentation/ui/signIn/SignInFragment.kt
@@ -9,10 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 import com.example.lesson_03_yermakov.R
+import com.example.lesson_03_yermakov.core.InputLayoutTextWatcher
 import com.example.lesson_03_yermakov.databinding.FragmentSignInBinding
 import com.google.android.material.snackbar.Snackbar
+import java.util.regex.Pattern
 
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
@@ -40,10 +42,44 @@ class SignInFragment : Fragment() {
                 }, 5000)
             }
 
+            textLogin.addTextChangedListener(
+                InputLayoutTextWatcher(
+                    layoutLogin,
+                    android.util.Patterns.EMAIL_ADDRESS,
+                    getString(R.string.input_base_error)
+                )
+            )
+
+            val passwordREGEX = Pattern.compile(
+                "^" +
+                        "(?=.*[0-9])" +         //at least 1 digit
+                        "(?=.*[a-z])" +         //at least 1 lower case letter
+                        "(?=.*[A-Z])" +         //at least 1 upper case letter
+                        "(?=.*[a-zA-Z])" +      //any letter
+                        "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                        "(?=\\S+$)" +           //no white spaces
+                        ".{8,}" +               //at least 8 characters
+                        "$"
+            )
+            textPassword.addTextChangedListener(
+                InputLayoutTextWatcher(
+                    layoutPassword,
+                    passwordREGEX,
+                    getString(R.string.input_base_error)
+                )
+            )
+
             textPassword.setOnEditorActionListener { _, actionId, event ->
-                if (actionId == EditorInfo.IME_ACTION_DONE || event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER) {
+                if (actionId == EditorInfo.IME_ACTION_DONE
+                    || event.action == KeyEvent.ACTION_DOWN
+                    && event.keyCode == KeyEvent.KEYCODE_ENTER
+                ) {
                     loading.visibility = View.VISIBLE
                     buttonSignIn.text = ""
+                    // Имитирую ожидание ответа от сервера.
+                    // По гайдам отображение прогрессбара делают с использованием стейтов,
+                    // потом перепишу правильно, если потребуется.
+                    showSnackbarError(view)
                     Handler(Looper.getMainLooper()).postDelayed({
                         navigateToCatalog()
                     }, 5000)
@@ -58,7 +94,7 @@ class SignInFragment : Fragment() {
     private fun navigateToCatalog() {
         binding.loading.visibility = View.INVISIBLE
         binding.buttonSignIn.setText(R.string.sign_in_action)
-        Navigation.findNavController(binding.root)
+        findNavController(binding.root)
             .navigate(R.id.action_sign_in_fragment_to_catalog_fragment)
     }
 
